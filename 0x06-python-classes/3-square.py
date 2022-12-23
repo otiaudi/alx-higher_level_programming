892f437c60613826f29c7654336168ea1326be2a
audi <otiaudi9@gmail.com>
2022-12-24 00:25:19 +0300

    implemented a class method that calculate area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
+
 """Class to represent square"""
 
 
 class Square:
     """Class that defines a suare"""
 
+    def __init__(self, size=0):
+        """Initializing object variable.
 
-def __init__(self, size=0):
-    """Initializing object variable.
-
-    args:
-        size (int): size of the new square
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
+        args:
+            size (int): size of the new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
         raise ValueError("size must be >= 0")
-    else:
-        self.__size = size
+        else:
+            self.__size = size
 
 
-def area(self):
-    """Function that calculates area of a square
-    """
-    return (self.__size ** 2)
+    def area(self):
+        """Function that calculates area of a square"""
+        return (self.__size * self.__size)
