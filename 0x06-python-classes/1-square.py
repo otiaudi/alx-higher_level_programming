5495a95f8c4afb434541e78b34914ffd6974f7c5
audi <otiaudi9@gmail.com>
2022-12-23 21:46:46 +0300

    a class Square that defines a square by

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """consructor method
+
+        args:
+            size (int): size of the square.
+        """
+        self.__size = size
