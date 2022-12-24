3ceb2f9b143f28a1fb849a4bbc81ff530b4993af
audi <otiaudi9@gmail.com>
2022-12-24 23:48:30 +0300

    a class Square that defines a square by gettters and setters

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,42 +1,39 @@
-#!/bin/python3
-"""Class blue print"""
+#!/usr/bin/python3
+
+"""Define a class Square."""
 
 
 class Square:
-    """class to create objects"""
-    def __init__(self, size=0):
-        """Method to initialize the size variable.
-        args:
-            size (int): size of the square.
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
         """
         self.size = size
 
     @property
     def size(self):
-        """Method to retrieve instance variable size"""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """method to set instance variable"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     def area(self):
-        """Method that returns the area of a square"""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Method that print the square of with the character '#'"""
-        if not self.__size:
-            print()
-            print("--")
-        else:
-            for i in range(0, self.__size):
-                for j in range(0, self.__size):
-                    print("#", end='')
-                print()
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
